3456a8c092f169aed7fb195a71df6e95220331e3
RuanDawnLord <xiaojun.ruan@gmail.com>
2017-08-31 17:26:48 -0700

    Algorithm Compexity on N Figure

--- Complexity of Algorithms.py
+++ Complexity of Algorithms.py
@@ -9,7 +9,7 @@ import numpy
 
 x = numpy.linspace(1,100,1000) # 100 linearly spaced numbers
 
-pylab.ylim([1,500])
+pylab.ylim([1,60])
 pylab.xlim([1,20])
 
 
@@ -20,6 +20,6 @@ pylab.plot(x, numpy.square(x), label='N^2')
 pylab.plot(x, numpy.power(x, 3), label='N^3')
 pylab.plot(x, numpy.power(2, x), label='2^N')
 
-pylab.legend(loc='upper left')
+pylab.legend(loc='upper right')
 pylab.title("Algorithm Complexity on N")
 pylab.show()

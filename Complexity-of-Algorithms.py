28192e09f8345f9c88e6b0e663d8f5ea9270d0ba
RuanDawnLord <xiaojun.ruan@gmail.com>
2017-09-01 17:14:44 -0700

    Change Lines order
    Lines are ordered as Most Comlex to less Complex

--- Complexity-of-Algorithms.py
+++ Complexity-of-Algorithms.py
@@ -12,12 +12,14 @@ pylab.ylim([1,60])
 pylab.xlim([1,20])
 
 
-pylab.plot(x, numpy.log2(x), label='log2(N)')
+pylab.plot(x, numpy.power(2, x), label='2^N')
+pylab.plot(x, numpy.power(x, 3), label='N^3')
+pylab.plot(x, numpy.square(x), label='N^2')
 pylab.plot(x, numpy.multiply(x,numpy.log2(x)), label='Nlog2(N)')
 pylab.plot(x, x, label='N')
-pylab.plot(x, numpy.square(x), label='N^2')
-pylab.plot(x, numpy.power(x, 3), label='N^3')
-pylab.plot(x, numpy.power(2, x), label='2^N')
+pylab.plot(x, numpy.log2(x), label='log2(N)')
+
+
 
 pylab.legend(loc='upper right')
 pylab.title("Algorithm Complexity on N")
